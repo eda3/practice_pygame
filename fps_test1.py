4924f7ed03dc4e96fe0021204839c276f3b2ef49
eda3 <gerorin1010@gmail.com>
2019-01-16 19:14:16 +0900

    first commit fps_test1.py

---
+++ fps_test1.py
@@ -0,0 +1,14 @@
+""" fps_test1.py"""
+import sys
+import pygame
+from pygame.locals import QUIT
+
+pygame.init()
+SURFACE = pygame.display.set_mode((400, 300))
+
+def main():
+    """ main routine """
+    pass
+
+if __name__ == "__main__":
+    main()
