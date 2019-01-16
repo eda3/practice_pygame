5fdc4f66caa4130ad0b816de4fb963f7eb9b7a24
eda3 <gerorin1010@gmail.com>
2019-01-16 19:02:36 +0900

    justwindows_pyxel.py first commit

---
+++ justwindows_pyxel.py
@@ -0,0 +1,14 @@
+import pyxel
+
+class App:
+    def __init__(self):
+        pyxel.init(255, 255)
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+App()
