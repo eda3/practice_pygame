f5b1ef218e9fd86b164127a8cee795f552dddb47
eda3 <gerorin1010@gmail.com>
2019-01-16 19:43:55 +0900

    add proccessing speed drawing

--- fps_test1.py
+++ fps_test1.py
@@ -8,7 +8,19 @@ SURFACE = pygame.display.set_mode((400, 300))
 
 def main():
     """ main routine """
-    pass
+    sysfont = pygame.font.SysFont(None, 36)
+    counter = 0
+    while True:
+        for event in pygame.event.get(QUIT):
+            pygame.quit()
+            sys.exit()
+
+        counter += 1
+        SURFACE.fill((0, 0, 0))
+        count_image = sysfont.render("count is {}".format(counter), True, (225, 225, 225))
+        SURFACE.blit(count_image, (50, 50))
+        pygame.display.update()
+
 
 if __name__ == "__main__":
     main()
