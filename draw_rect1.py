a6e7e5ad9a39886dac5c2bd1bdba4708beedb594
eda3 <gerorin1010@gmail.com>
2019-01-20 21:14:59 +0900

    first commit

---
+++ draw_rect1.py
@@ -0,0 +1,16 @@
+""" draw_rect1.py """
+import sys
+import pygame
+from pygame.locals import QUIT, Rect
+
+pygame.init()
+SURFACE = pygame.display.set_mode((400, 300))
+FPSCLOCK = pygame.time.Clock()
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
