7da9db9a969594f18e35cd24b48fbf3d2a44c69a
eda3 <gerorin1010@gmail.com>
2019-01-20 16:07:16 +0900

    fix: refactoring

--- fps_test1.py
+++ fps_test1.py
@@ -6,6 +6,7 @@ from pygame.locals import QUIT
 pygame.init()
 SURFACE = pygame.display.set_mode((400, 300))
 
+
 def main():
     """ main routine """
     sysfont = pygame.font.SysFont(None, 36)
@@ -17,7 +18,9 @@ def main():
 
         counter += 1
         SURFACE.fill((0, 0, 0))
-        count_image = sysfont.render("count is {}".format(counter), True, (225, 225, 225))
+        color = (225, 225, 225)
+        message = "count is {}".format(counter)
+        count_image = sysfont.render(message, True, color)
         SURFACE.blit(count_image, (50, 50))
         pygame.display.update()
 
