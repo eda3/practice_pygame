df51d32876839a91184c62140dd76ae194f01e6c
eda3 <gerorin1010@gmail.com>
2019-01-20 21:05:48 +0900

    add: counter message text

--- fps_test1_pyxel.py
+++ fps_test1_pyxel.py
@@ -2,15 +2,23 @@ import pyxel
 
 
 class App:
+    __counter = 0
+
     def __init__(self):
         pyxel.init(255, 255)
-        pyxel.run()
+        pyxel.cls(1)
+        pyxel.run(self.update, self.draw)
 
     def update(self):
-        pass
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+
+        self.__counter += 1
 
     def draw(self):
-        pass
+        _color = 7
+        _message = "count is {}".format(self.__counter)
+        pyxel.text(50, 50, _message, _color)
 
 
 App()
