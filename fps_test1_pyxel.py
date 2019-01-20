dc9f7f1452a7410601193e449b6a43145c13b397
eda3 <gerorin1010@gmail.com>
2019-01-20 16:10:56 +0900

    first commit

---
+++ fps_test1_pyxel.py
@@ -0,0 +1,16 @@
+import pyxel
+
+
+class App:
+    def __init__(self):
+        pyxel.init(255, 255)
+        pyxel.run()
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+App()
