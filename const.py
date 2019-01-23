1e5c901aac10cb295aafe97dcb444a4af4b7fece
eda3 <gerorin1010@gmail.com>
2019-01-23 17:38:11 +0900

    first commit

---
+++ const.py
@@ -0,0 +1,20 @@
+from enum import IntEnum
+
+
+class Color(IntEnum):
+    BLACK = 0
+    NAVY_BLUE = 1
+    WINE_RED = 2
+    FOREST_GREEN = 3
+    BROWN = 4
+    SILVER_GREY = 5
+    ASH_GREY = 6
+    AMBER_WHITE = 7
+    RED = 8
+    ORANGE = 9
+    YELLOW = 10
+    LIME_GREEN = 11
+    SYAN_BLUE = 12
+    OLD_ROSE = 13
+    PINK = 14
+    PEACH = 15
