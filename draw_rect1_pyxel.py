3fcae2c5668dc569c2cb670d0334c990083c4a22
eda3 <gerorin1010@gmail.com>
2019-01-28 10:33:17 +0900

    add: add rectangles

--- draw_rect1_pyxel.py
+++ draw_rect1_pyxel.py
@@ -15,14 +15,19 @@ class App:
         pyxel.cls(Color.PEACH)
 
         # Red:Rectangle(fill)
+        pyxel.rect(10, 20, 120, 70, Color.RED)
 
         # Red:Rectangle(line weight 3)
+        pyxel.rectb(150, 10, 250, 40, Color.RED)
 
         # Green:Rectangle
+        pyxel.rect(100, 80, 180, 130, Color.LIME_GREEN)
 
         # Blue:Rectangle,Rect Object
+        pyxel.rect(200, 60, 340, 120, Color.SYAN_BLUE)
 
         # Yellow:Rectangle,Rect Object
+        pyxel.rect(30, 160, 130, 210, Color.YELLOW)
 
         pass
 
