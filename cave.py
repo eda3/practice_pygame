fe4e93ec562ea4958a33d064be086d7fbbfd8035
eda3 <gerorin1010@gmail.com>
2019-01-28 20:02:30 +0900

    first commit

---
+++ cave.py
@@ -0,0 +1,27 @@
+""" cave.py """
+import sys
+import pygame
+from pygame.locals import QUIT
+from pygame import Surface
+
+pygame.init()
+SURFACE = pygame.display.set_mode((400, 300))  # type:Surface
+FPSCLOCK = pygame.time.Clock()
+
+
+def main():
+    """ main routine """
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit
+
+        SURFACE.fill((255, 255, 255))
+
+        pygame.display.update()
+        FPSCLOCK.tick(30)
+
+
+if __name__ == "__main__":
+    main()
