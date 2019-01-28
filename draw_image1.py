0783959bd6ea75be316abc9f840148c6e2421780
eda3 <gerorin1010@gmail.com>
2019-01-28 10:52:02 +0900

    first commit

---
+++ draw_image1.py
@@ -0,0 +1,26 @@
+""" draw_image1.py """
+import sys
+import pygame
+from pygame.locals import QUIT
+
+pygame.init()
+SURFACE = pygame.display.set_mode((400, 300))
+FPSCLOCK = pygame.time.Clock()
+
+
+def main():
+    """ main routine """
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit
+
+        SURFACE.fill((255, 255, 255))
+
+        pygame.display.update()
+        FPSCLOCK.tick(30)
+
+
+if __name__ == "__main__":
+    main()
