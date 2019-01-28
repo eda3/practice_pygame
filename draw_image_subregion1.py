b0d182e904f3391bb85603aa356dacbbaf871461
eda3 <gerorin1010@gmail.com>
2019-01-28 14:02:22 +0900

    first commit

---
+++ draw_image_subregion1.py
@@ -0,0 +1,33 @@
+""" draw_image1.py """
+import sys
+
+import pygame
+from pygame import Surface
+from pygame.locals import QUIT
+
+pygame.init()
+SURFACE = pygame.display.set_mode((400, 300))  # type: Surface
+FPSCLOCK = pygame.time.Clock()
+
+
+def main():
+    """ main routine """
+    logo = pygame.image.load("./assets/pythonlogo.png")  # type: Surface
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit
+
+        SURFACE.fill((255, 255, 255))
+
+        # draw a logo at the (20, 50) upper left corner
+        SURFACE.blit(logo, (0, 0))
+
+        pygame.display.update()
+        FPSCLOCK.tick(30)
+
+
+if __name__ == "__main__":
+    main()
