05db86ed9f5780c2971973f1fe33b3091d9d9ee0
eda3 <gerorin1010@gmail.com>
2019-01-28 18:20:25 +0900

    add: draw circle on mousebutton down

--- draw_circle_onclick.py
+++ draw_circle_onclick.py
@@ -1,7 +1,7 @@
 """ draw_circle_onclick.py"""
 import sys
 import pygame
-from pygame.locals import QUIT
+from pygame.locals import QUIT, MOUSEBUTTONDOWN
 from pygame import Surface
 
 pygame.init()
@@ -11,13 +11,19 @@ FPSCLOCK = pygame.time.Clock()
 
 def main():
     """ main routine """
+    mousepos = []
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit
+            elif event.type == MOUSEBUTTONDOWN:
+                mousepos.append(event.pos)
 
         SURFACE.fill((255, 255, 255))
+
+        for i, j in mousepos:
+            pygame.draw.circle(SURFACE, (0, 255, 0), (i, j), 5)
 
         pygame.display.update()
         FPSCLOCK.tick(30)
