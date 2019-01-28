bb8116dc5b5bbca661d7be7483a196d5df795543
eda3 <gerorin1010@gmail.com>
2019-01-28 19:04:49 +0900

    add: draw line on mouse move

--- draw_line_onmousemove.py
+++ draw_line_onmousemove.py
@@ -1,7 +1,7 @@
 """ draw_line_onmousemove.py """
 import sys
 import pygame
-from pygame.locals import QUIT
+from pygame.locals import QUIT, MOUSEBUTTONDOWN, MOUSEMOTION, MOUSEBUTTONUP
 from pygame import Surface
 
 pygame.init()
@@ -11,13 +11,26 @@ FPSCLOCK = pygame.time.Clock()
 
 def main():
     """ main routine """
+    mousepos = []
+    mousedown = False
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit
+            elif event.type == MOUSEBUTTONDOWN:
+                mousedown = True
+            elif event.type == MOUSEMOTION:
+                if mousedown:
+                    mousepos.append(event.pos)
+            elif event.type == MOUSEBUTTONUP:
+                mousedown = False
+                mousepos.clear()
 
         SURFACE.fill((255, 255, 255))
+
+        if len(mousepos) > 1:
+            pygame.draw.lines(SURFACE, (255, 0, 0), False, mousepos)
 
         pygame.display.update()
         FPSCLOCK.tick(30)
