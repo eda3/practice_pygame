f8277165059742ed9fd34245351d41f0a5f178d0
eda3 <gerorin1010@gmail.com>
2019-01-28 19:22:07 +0900

    add: draw the pentagon

--- draw_lines1.py
+++ draw_lines1.py
@@ -1,5 +1,6 @@
 """ draw_lines1.py"""
 import sys
+from math import sin, cos, radians
 import pygame
 from pygame.locals import QUIT
 from pygame import Surface
@@ -17,7 +18,16 @@ def main():
                 pygame.quit()
                 sys.exit
 
-        SURFACE.fill((255, 255, 255))
+        SURFACE.fill((0, 0, 0))
+
+        pointlist0, pointlist1 = [], []
+        for theta in range(0, 720, 144):
+            rad = radians(theta)
+            pointlist0.append((cos(rad) * 100 + 100, sin(rad) * 100 + 150))
+            pointlist1.append((cos(rad) * 100 + 300, sin(rad) * 100 + 150))
+
+        pygame.draw.lines(SURFACE, (255, 255, 255), True, pointlist0, 5)
+        pygame.draw.lines(SURFACE, (255, 255, 255), True, pointlist1)
 
         pygame.display.update()
         FPSCLOCK.tick(30)
