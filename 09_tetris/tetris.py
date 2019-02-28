21cf58c693aae4b1de2443879409706f95298ed4
eda3 <gerorin1010@gmail.com>
2019-02-28 13:11:59 +0900

    add: add global variables

--- 09_tetris/tetris.py
+++ 09_tetris/tetris.py
@@ -126,6 +126,20 @@ def is_overlapped(xpos, ypos, turn):
     pass
 
 
+pygame.init()
+pygame.key.set_repeat(30, 30)
+SURFACE = pygame.display.set_mode([600, 600])
+FPSCLOCK = pygame.time.Clock()
+WIDTH = 12
+HEIGHT = 22
+INTERVAL = 40
+FIELD = [[0 for _ in range(WIDTH)] for _ in range(HEIGHT)]
+COLORS = ((0, 0, 0), (255, 165, 0), (0, 0, 255), (0, 255, 255), (0, 255, 0),
+          (255, 0, 255), (255, 255, 0), (255, 0, 0), (128, 128, 128))
+BLOCK = None
+NEXT_BLOCK = None
+
+
 def main():
     pass
 
